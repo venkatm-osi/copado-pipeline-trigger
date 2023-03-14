4d97bbe20034743d370f04a791f6acd7bf7a658e
Venkat Malipeddi <110934583+venkatm-osi@users.noreply.github.com>
2023-03-14 15:47:12 -0700

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,10 +1,13 @@
 def lambda_handler(event, context):
     import json
 
+    if event["httpMethod"] != "POST":
+        return {
+            "statusCode": 405,
+            "body": "Method not allowed",
+        }
+
     try:
-        if event["httpMethod"] != "POST":
-            raise Exception("Invalid HTTP method")
-        
         apiKey = event["headers"]["X-API-Key"]
         if apiKey != "YOUR_API_KEY":
             raise Exception("Invalid API key")
@@ -24,29 +27,7 @@ def lambda_handler(event, context):
             "body": "Missing API key in request headers",
         }
     except Exception as e:
-        if str(e) == "Invalid HTTP method":
-            return {
-                "statusCode": 405,
-                "body": "Method not allowed",
-            }
-        else:
-            return {
-                "statusCode": 401,
-                "body": str(e),
-            }
-
-def start_code_pipeline(pipelineName):
-    client = codepipeline_client()
-    response = client.start_pipeline_execution(name=pipelineName)
-    return True
-
-cpclient = None
-
-def codepipeline_client():
-    import boto3
-
-    global cpclient
-    if not cpclient:
-        cpclient = boto3.client("codepipeline")
-    return cpclient
-]
+        return {
+            "statusCode": 401,
+            "body": str(e),
+        }
