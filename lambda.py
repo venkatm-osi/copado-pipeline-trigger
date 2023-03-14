e704479bde2beb73086ad8971561754e65ca832e
Venkat Malipeddi <110934583+venkatm-osi@users.noreply.github.com>
2023-03-14 15:39:21 -0700

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,18 +1,39 @@
 def lambda_handler(event, context):
     import json
 
-    requestedPipeline = event["message"][0]["keyword"]
-
-    #start the pipeline
-    if len(requestedPipeline)>0:
-        # Codepipeline name is foldername-job.
-        # We can read the configuration from S3 as well.
+    try:
+        if event["httpMethod"] != "POST":
+            raise Exception("Invalid HTTP method")
+        
+        apiKey = event["headers"]["X-API-Key"]
+        if apiKey != "YOUR_API_KEY":
+            raise Exception("Invalid API key")
+        
+        requestedPipeline = event["message"][0]["keyword"]
+        if len(requestedPipeline) == 0:
+            raise Exception("No pipeline name provided")
+        
         returnCode = start_code_pipeline(requestedPipeline)
-
-    return {
-        "statusCode": 200,
-        "body": requestedPipeline,
-    }
+        return {
+            "statusCode": 200,
+            "body": requestedPipeline,
+        }
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": "Missing API key in request headers",
+        }
+    except Exception as e:
+        if str(e) == "Invalid HTTP method":
+            return {
+                "statusCode": 405,
+                "body": "Method not allowed",
+            }
+        else:
+            return {
+                "statusCode": 401,
+                "body": str(e),
+            }
 
 def start_code_pipeline(pipelineName):
     client = codepipeline_client()
@@ -28,3 +49,4 @@ def codepipeline_client():
     if not cpclient:
         cpclient = boto3.client("codepipeline")
     return cpclient
+]
